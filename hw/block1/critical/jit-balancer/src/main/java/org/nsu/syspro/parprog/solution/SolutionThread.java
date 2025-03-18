ec41a34437213c08c0616f73f98837bfd93af17d
bogdan <b.pyanzin@mail.ru>
2025-03-18 10:00:07 +0700

    First review

--- hw/block1/critical/jit-balancer/src/main/java/org/nsu/syspro/parprog/solution/SolutionThread.java
+++ hw/block1/critical/jit-balancer/src/main/java/org/nsu/syspro/parprog/solution/SolutionThread.java
@@ -5,14 +5,22 @@ import org.nsu.syspro.parprog.external.*;
 
 
 import java.util.concurrent.*;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/***
+ * Решение использует глобальное кэширование из 2-х кэшей для методов( метод - (уровень, скомп.метод))
+ * и счётчик кол-ва вызовов метода. Они представлены ConcurrentHashMap и операции get, put безопасны.<br>
+ * Описание решения:<br>
+ * Увеличивается стётчик вызовов, проверяется кэш методов:<br>
+ *  если >10_000 и уровень компиляции меньше L2, то происходит асинхронная компиляция до L2.<br>
+ *  если >5_000 && <=10_000 и уровень меньше L1, то асинхронная компиляция до L1.<br>
+ *  в противном случае метод остаётся интерпретируемым.<br>
+ * Если метод есть в кэше он исполняется, иначе интерпретируется<br>
+ * В процессе асинхронной компиляции происходит обновление кэша методов.<br>
+ */
 public class SolutionThread extends UserThread {
     // TODO: add fields here!
     private static final ConcurrentHashMap<MethodID, CompiledMethodInfo> cachedCompiledMethods = new ConcurrentHashMap<>();
     private final ConcurrentHashMap<MethodID, Long> hotness = new ConcurrentHashMap<>();
-    private final ReentrantReadWriteLock rwlMethods = new ReentrantReadWriteLock();
-    private final ReentrantReadWriteLock rwlHotness = new ReentrantReadWriteLock();
     private final CompilationExecution multiThreadCompiler;
 
     public SolutionThread(int compilationThreadBound, ExecutionEngine exec, CompilationEngine compiler, Runnable r) {
@@ -24,9 +32,9 @@ public class SolutionThread extends UserThread {
 
     @Override
     public ExecutionResult executeMethod(MethodID id) {
-        final long hotLevel = getHotness(id);
-        putHotness(id, hotLevel + 1);
-        CompiledMethodInfo cachedInfo = getMethods(id);
+        final long hotLevel = hotness.getOrDefault(id, 0L);
+        hotness.put(id, hotLevel + 1);
+        CompiledMethodInfo cachedInfo = cachedCompiledMethods.get(id);
         CompilationLevel level = null;
 
         if (hotLevel > 10_000 && cachedInfo != null && cachedInfo.level.ordinal() < CompilationLevel.L2.ordinal()) {
@@ -34,15 +42,11 @@ public class SolutionThread extends UserThread {
         } else if (hotLevel > 5_000 && cachedInfo == null) {
             level = CompilationLevel.L1;
         }
-        //lock.lock();
-        //synchronized (in) {
+
         if (level != null) {
-            CompiledMethod method = multiThreadCompiler.compile(id, level);
-            putMethods(id, new CompiledMethodInfo(method, level));
-            return exec.execute(method);
+            multiThreadCompiler.asynCompile(id, level);
         }
-        //}
-        //lock.unlock();
+        cachedInfo = cachedCompiledMethods.get(id);
         return cachedInfo != null
                 ? exec.execute(cachedInfo.method)
                 : exec.interpret(id);
@@ -55,42 +59,6 @@ public class SolutionThread extends UserThread {
         L1, L2
     }
 
-    private Long getHotness(MethodID key) {
-        rwlHotness.readLock().lock();
-        try {
-            Long v;
-            return (v = hotness.get(key)) == null ? 0L : v;
-        } finally {
-            rwlHotness.readLock().unlock();
-        }
-    }
-
-    private void putHotness(MethodID key, Long value) {
-        rwlHotness.writeLock().lock();
-        try {
-            hotness.put(key, value);
-        } finally {
-            rwlHotness.writeLock().unlock();
-        }
-    }
-
-    private CompiledMethodInfo getMethods(MethodID key) {
-        rwlMethods.readLock().lock();
-        try {
-            return cachedCompiledMethods.get(key);
-        } finally {
-            rwlMethods.readLock().unlock();
-        }
-    }
-
-    private void putMethods(MethodID key, CompiledMethodInfo value) {
-        rwlMethods.writeLock().lock();
-        try {
-            cachedCompiledMethods.put(key, value);
-        } finally {
-            rwlMethods.writeLock().unlock();
-        }
-    }
 
     private static class CompiledMethodInfo {
         public final CompiledMethod method;
@@ -113,21 +81,25 @@ public class SolutionThread extends UserThread {
             executor = Executors.newFixedThreadPool(compilationThreadBound);
         }
 
-        public CompiledMethod compile(MethodID id, CompilationLevel level) {
-            Callable<CompiledMethod> compileCallable;
-            if (level == CompilationLevel.L1) {
-                compileCallable = () -> engine.compile_l1(id);
-            } else {
-                compileCallable = () -> engine.compile_l2(id);
-            }
-            Future<CompiledMethod> future = executor.submit(compileCallable);
-            CompiledMethod code;
-            try {
-                code = future.get();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            return code;
+        public void asynCompile(MethodID id, CompilationLevel level) {
+            CompletableFuture.runAsync(() -> {
+                try {
+                    CompiledMethod method = (level == CompilationLevel.L1)
+                            ? engine.compile_l1(id)
+                            : engine.compile_l2(id);
+
+                    cachedCompiledMethods.compute(id, (k, oldInfo) -> {
+                        if (oldInfo == null || level.ordinal() > oldInfo.level.ordinal()) {
+                            return new CompiledMethodInfo(method, level);
+                        }
+                        return oldInfo;
+                    });
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }, executor);
+
         }
     }
 }
